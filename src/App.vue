baefa58773d1028b0e3372a57bbce84d4f67c981
Marlee Alcaraz <marlee.alcaraz@gmail.com>
2022-11-08 21:19:49 +0100

    commit4

--- src/App.vue
+++ src/App.vue
@@ -5,74 +5,91 @@ import  Forecast from './components/Forecast.vue';
 <script>
 const apiKey = "f354617bca59ded0ad175d1fb6426bc7";
 
-// window.fetch('http://api.openweathermap.org/data/2.5/weather?lat=43.296482&lon=5.36978&lang=fr&units=metric&appid=f354617bca59ded0ad175d1fb6426bc7')
+window.fetch('http://api.openweathermap.org/data/2.5/weather?lat=43.296482&lon=5.36978&lang=fr&units=metric&appid=f354617bca59ded0ad175d1fb6426bc7')
 
-// .then(res => res.json())
-// .then(resJson => console.log(resJson))
+.then(res => res.json())
+.then(resJson => console.log(resJson))
 
 
 export default {
   data () {
     return{
-      datetime:"",
-      city:"",
-      tempMax:"",
-      tempMin:"",
-      vitVent:"",
-      description:"",
-      icon:""
+      listCards:[]
     }
   },
 mounted : async function(){
-//   window.fetch('http://api.openweathermap.org/data/2.5/weather?lat=43.296482&lon=5.36978&lang=fr&units=metric&appid=f354617bca59ded0ad175d1fb6426bc7')
 
-// .then(res => res.json())
-// .then(resJson => console.log(resJson.main.temp_min))
+const res = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=43.1642&lon=5.2219&appid=${apiKey}&units=metric&lang=fr`)
 
-const res = await fetch('http://api.openweathermap.org/data/2.5/weather?lat=43.296482&lon=5.36978&lang=fr&units=metric&appid=f354617bca59ded0ad175d1fb6426bc7')
-const json = await res.json()
-console.log(json);
-this.city= json.name
-console.log(json.name);
+const data = await res.json();
+    this.listCards = data.list;
+    console.log("listCards", this.listCards);
 
-this.tempMin= json.main.temp_min
-console.log(json.main.temp_min);
+// this.tempMin= json.main.temp_min
+// console.log(json.main.temp_min);
 
-this.tempMax= json.main.temp_max
-console.log(json.main.temp_max);
+// this.tempMax= json.main.temp_max
+// console.log(json.main.temp_max);
 
-this.vitVent= json.wind.speed
-console.log(json.wind.speed);
+// this.vitVent= json.wind.speed
+// console.log(json.wind.speed);
 
-this.description= json.weather[0].description
-console.log(json.weather[0].description);
+// this.description= json.weather[0].description
+// console.log(json.weather[0].description);
 
-this.icon= json.weather[0].icon
+// this.icon= json.weather[0].icon
 }
 }
+
 
 </script>
 
 <template>
   <header>
-    <h1>Application météo</h1>
+    <h1>La météo de Marseille</h1>
   </header>
 
-  <div>
-   <Forecast 
-   :city= this.city 
-   :tempMax= this.tempMax 
-   :tempMin= this.tempMin 
-   :vitVent= this.vitVent 
-   :description= this.description 
-   :icon= this.icon /> 
-  </div>
+  <main>
+
+    
+     <Forecast 
+     v-for="forecast in listCards"
+        :description="forecast.weather[0].description"
+        :vitVent="forecast.wind.speed"
+        :tempMin="forecast.main.temp_min"
+        :tempMax="forecast.main.temp_max"
+        :datetime="forecast.dt_txt"
+        :icon="forecast.weather[0].icon"
+        :key="forecast.dt" /> 
+  
+  </main>
+
+  <footer>&copy;Marlee Météo</footer>
  
 </template>
 
 <style scoped>
-header{
+
+header,footer{
+
+  height: 5rem;
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    padding: 0 1rem;
+    background-color: rgb(106, 182, 198);
+    color: white;
+}
+main {
   width: 100%;
-  flex-direction: column;
+  flex-grow: 1;
+  padding: 1rem;
+  display: flex;
+  flex-direction: row;
+  gap: 1rem;
+  flex-wrap: wrap;
+  justify-content: space-around;
 }
 </style>
--- src/components/Forecast.vue
+++ src/components/Forecast.vue
@@ -1,50 +1,89 @@
 <script>
 export default {
-  props: ['datetime','city','tempMax', 'tempMin', 'vitVent', 'description','icon'],
+  props: [
+    "datetime",
+    "city",
+    "tempMax",
+    "tempMin",
+    "vitVent",
+    "description",
+    "icon",
+  ],
 };
 </script>
 
 <template>
+  
   <div class="meteoCard">
-    <h1>{{city}}</h1>
-    <img :src="`http://openweathermap.org/img/wn/${icon}@2x.png`" alt="">
-    <h2>{{description}}</h2>
-    <p>{{ datetime}}</p>
+    <h1>{{ city }}</h1>
+    <img :src="`http://openweathermap.org/img/wn/${icon}@2x.png`" alt="" />
+    <h2>{{ description }}</h2>
+    <p>{{ datetime }}</p>
     <div>
-      <h4>Températures</h4>
-      <p>Min: {{tempMin}}°C</p>
-      <p>Max: {{tempMax}}°C</p>
-
-      <div>
-        <h4>Vitesse du vent</h4>
-        <p>{{vitVent}}km/h</p>
+      <div class="infos">
+        <div>
+          <h4>Températures</h4>
+          <p>Min: {{ tempMin }}°C</p>
+          <p>Max: {{ tempMax }}°C</p>
+        </div>
+        <div>
+          <h4>Vitesse du vent</h4>
+          <p>{{ vitVent }}km/h</p>
+        </div>
       </div>
+
     </div>
-    
   </div>
 </template>
 
-<!-- et l'icone --> 
+<!-- et l'icone -->
 
 <style scoped>
+
 .meteoCard {
+  margin: 15px;
+  border: 1px solid black;
+  padding: 1rem;
   display: flex;
   flex-direction: column;
-  justify-content: space-around;
-  width: 250px;
-  height: 350px;
-  border: solid 1px grey;
-  border-radius: 20px;
-  text-align:center;
-  font-size: 18px;
-  padding: 20px;
-
-}
-img{
+  align-items: center;
+  height: fit-content;
   
+}
+p:first-child {
+  margin: 0;
+  padding: 1rem;
+  background-color: #747474;
+  color: white;
+}
+img {
   width: 100px;
   height: 100px;
   margin-left: 50px;
-  
+}
+img {
+  width: 100px;
+  height: 100px;
+}
+.infos {
+  display: flex;
+}
+p,
+h4 {
+  margin: 0;
+}
+h4 {
+  box-sizing: border-box;
+  margin-bottom: 0.75rem;
+  border-bottom: 1px solid black;
+}
+div:first-child {
+  padding-right: 0.5rem;
+  border-right: 1px solid black;
+  border-left: 1px solid black;
+ 
+}
+div:last-child {
+  padding-left: 0.5rem;
 }
 </style>
